04f0edd9c9518f1c263366e978be8f70f3bcbc4e
emilievdweele <74767348+emilievdweele@users.noreply.github.com>
2021-06-11 15:36:55 +0000

    Loop van het spel + feedback en variabelen verplaatst

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import random
+geheimecode = ""
+
+#Aantal variabelen vaststellen
+aantal_kleuren = 4
+aantal_beurten = 10
+kleuren = ["r", "b", "g", "c", "p", "m"]
 
 #Computer kiest geheime code
 def kiesgeheimecode(kleuren):
@@ -16,35 +22,10 @@ def geeffeedback(gok, geheimecode):
 
 #Kopie geheime code (list) voor juiste feedback
 kopiegok = []
-for i in range(len(geheimeCode)):
-    kopiegok.append(geheimecode[i])
+for i in range(len(geheimecode)):
+    kopiegok.append(geheimecode [i])
 
 feedback = []
-
-#Eerste deel feedback: zitten geraden kleuren op de goede plek?
-#Naar x veranderen, dus geen duplicaties voor tweede deel feedback
-for i in range(aantal_kleuren):
-    if gok[i] == kopiegok[i]:
-      feedback.append("Z")
-      kopiegok[i] = "X"
-
-#Tweede deel feedback: zitten de geraden kleuren in de code?
-#Naar x veranderen, dus geen duplicaties
-for gokletter in range(aantal_kleuren):
-    for codeletter in range(len(kopiegok)):
-      if gok[gokletter] == kopiegok[codeletter]:
-        feedback.append("W")
-        kopiegok[codeletter] = "X"
-
-  if len(feedback) == 0:
-    return "Helaas komen geen van de geraden kleuren in de code voor! Probeer het nog eens!
-  else:
-    return "".join(feedback)
-
-#Aantal variabele vaststellen
-aantal_kleuren = 4
-aantal_beurten = 10
-kleuren = ["r", "b", "g", "c", "p", "m"]
 
 #Regel tekst
 print("Welkom bij mastermind!")
@@ -61,3 +42,39 @@ print()
 print("Je kan alleen de letters invullen van de kleuren waaruit je kan kiezen. Bij het invullen van meer, minder of andere letters, tekens of cijfers gaat er een beurt af.")
 print()
 print("Voorbeeld: ZWW, dit betekent dat er drie kleuren goed zijn geraden en er daarvan één op de goede plek zit en twee op de verkeerde plek.")
+
+#Loop van het spel
+while True:
+  beurt = 1
+  geheimeCode = kiesgeheimecode(kleuren)
+  print("Je hebt " + str(aantal_beurten) + " beurten")
+  while beurt <= aantal_beurten:
+    gokopnieuw = False
+    print("Beurt " + str(beurt))
+    gok = input().lower()
+    for i in gok:
+      if i.isdigit():
+        gokopnieuw = True
+      elif i not in kleuren:
+        gokopnieuw = True
+
+    print(geeffeedback(gok, geheimecode))
+    beurt += 1
+
+    if gok == geheimecode:
+      break
+
+#Eerste deel feedback: zitten geraden kleuren op de goede plek?
+#Naar x veranderen, dus geen duplicaties voor tweede deel feedback
+for i in range(aantal_kleuren):
+    if gok[i] == kopiegok[i]:
+      feedback.append("Z")
+      kopiegok[i] = "X"
+
+#Tweede deel feedback: zitten de geraden kleuren in de code?
+#Naar x veranderen, dus geen duplicaties
+for gokletter in range(aantal_kleuren):
+    for codeletter in range(len(kopiegok)):
+      if gok[gokletter] == kopiegok[codeletter]:
+        feedback.append("W")
+        kopiegok[codeletter] = "X"
